764d93608572039a909451b8665660736295997d
Jozue06 <jozue06@gmail.com>
2018-07-04 19:47:48 -0700

    s

--- config.js
+++ config.js
@@ -1,23 +1,40 @@
-var _ = require('underscore');
+var dotenv = require('dotenv');
+var cfg = {};
 
-var config = {
-  secret: process.env.APP_SECRET,
-
-  // Your Twilio account SID and auth token, both found at:
-  // https://www.twilio.com/user/account
-  accountSid: process.env.TWILIO_ACCOUNT_SID,
-  authToken: process.env.TWILIO_AUTH_TOKEN,
-  phoneNumber: process.env.TWILIO_NUMBER,
-  notificationServiceSid: process.env.TWILIO_NOTIFICATION_SERVICE_SID
-};
-
-var isUnconfigured = _.every(_.values(config), function(value) {
-  return _.isUndefined(value);
-});
-
-if (isUnconfigured) {
-  throw new Error(
-    'TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_NUMBER, and TWILIO_NOTIFICATION_SERVICE_SID must be set');
+if (process.env.NODE_ENV !== 'production' && process.env.NODE_ENV !== 'test') {
+  dotenv.config({path: '.env'});
+} else {
+  dotenv.config({path: '.env.example', silent: true});
 }
 
-module.exports = config;
+// HTTP Port to run our web application
+cfg.port = process.env.PORT || 3000;
+
+// A random string that will help generate secure one-time passwords and
+// HTTP sessions
+cfg.secret = process.env.APP_SECRET || 'keyboard cat';
+
+// Your Twilio account SID and auth token, both found at:
+// https://www.twilio.com/user/account
+//
+// A good practice is to store these string values as system environment
+// variables, and load them from there as we are doing below. Alternately,
+// you could hard code these values here as strings.
+cfg.accountSid = process.env.TWILIO_ACCOUNT_SID;
+cfg.authToken = process.env.TWILIO_AUTH_TOKEN;
+cfg.sendingNumber = process.env.TWILIO_NUMBER;
+
+var requiredConfig = [cfg.accountSid, cfg.authToken, cfg.sendingNumber];
+var isConfigured = requiredConfig.every(function(configValue) {
+  return configValue || false;
+});
+
+if (!isConfigured) {
+  var errorMessage =
+    'TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, and TWILIO_NUMBER must be set.';
+
+  throw new Error(errorMessage);
+}
+
+// Export configuration object
+module.exports = cfg;
